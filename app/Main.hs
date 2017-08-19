e1392b65fd7704276d6b33d6c2dbef5b0a077ac2
fishyFrogFace <camillahallo@mail.com>
2017-08-19 20:24:05 +0200

    Reads Multiset from list of numbers separated by space

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,13 @@
 module Main where
 
 import Lib
+import Ms
+
+readMs :: IO (Multiset Int)
+readMs = fmap fromList list
+            where list = fmap (map read . words) getLine
 
 main :: IO ()
-main = test
+main = do
+    deltaX <- readMs
+    print deltaX
