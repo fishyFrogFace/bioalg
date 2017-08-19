8eac05a30c35d53bfa94586d66be32ca038dc21c
fishyFrogFace <camillahallo@mail.com>
2017-08-19 20:22:31 +0200

    Refined Multiset and added show/fromList tests

--- src/Ms.hs
+++ src/Ms.hs
@@ -6,18 +6,19 @@ module Ms
 
 import Data.List (sort)
 
-data Multiset a = Multiset [a] | Empty
+data Multiset a = Multiset [a]
+                  deriving (Eq)
 
 instance Show a => Show (Multiset a) where
     show = mshow
     
 mshow :: Show a => Multiset a -> String
-mshow Empty = "ms{\216}"
+mshow (Multiset []) = "ms{\216}"
 mshow (Multiset elems) = "ms{" ++ show' elems
                         where show' :: Show a => [a] -> Prelude.String
                               show' [x] = Prelude.show x ++ "}"
                               show' (x:xs) = Prelude.show x ++ (',' : show' xs)
 
 fromList :: Ord a => [a] -> Multiset a
-fromList [] = Empty
+fromList [] = (Multiset [])
 fromList l = Multiset $ sort l
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,18 @@
+import Test.Hspec
+
+import Ms
+
+empty = [] :: [Int]
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "fromList" $ do
+        it "constructs a valdid multiset with items" $ do
+            fromList [1,2,5,5,4] `shouldBe` Multiset [1,2,4,5,5]
+        it "constructs a valid empty set" $ do
+            fromList empty `shouldBe` Multiset empty
+    describe "Show" $ do
+        it "shows an empty multiset" $ do
+            show (Multiset empty) `shouldBe` "ms{\216}"
+        it "shows a non-empty set" $ do
+            show (Multiset [1,2,3]) `shouldBe` "ms{1,2,3}"
